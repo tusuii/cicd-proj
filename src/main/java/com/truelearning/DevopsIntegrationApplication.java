fdfa0eb3201ca4de037b5139948719eb1e44bc77
subodh kamble <39451041+tusuii@users.noreply.github.com>
2025-05-02 02:31:32 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/truelearning/DevopsIntegrationApplication.java
+++ src/main/java/com/truelearning/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to True learning by Akshat";
+		return "welcome to my k8s devOps project";
 	}
 
 	public static void main(String[] args) {
